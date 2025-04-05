bfa1db282d93f78c47a22f0978275ce4e4badb81
Guilherme Campos <guilhermeozana@hotmail.com>
2025-04-04 23:11:04 -0300

    feat: add unit of work pattern

--- GymManagement.Application/Subscriptions/Commands/CreateSubscription/CreateSubscriptionCommandHandler.cs
+++ GymManagement.Application/Subscriptions/Commands/CreateSubscription/CreateSubscriptionCommandHandler.cs
@@ -1,12 +1,33 @@
 using MediatR;
 using ErrorOr;
+using GymManagement.Application.Common.Interfaces;
+using GymManagement.Domain.Subscriptions;
 
 namespace GymManagement.Application.Subscriptions.Commands.CreateSubscription;
 
-public class CreateSubscriptionCommandHandler : IRequestHandler<CreateSubscriptionCommand, ErrorOr<Guid>>
+public class CreateSubscriptionCommandHandler : IRequestHandler<CreateSubscriptionCommand, ErrorOr<Subscription>>
 {
-    public async Task<ErrorOr<Guid>> Handle(CreateSubscriptionCommand request, CancellationToken cancellationToken)
+    private readonly ISubscriptionsRepository _subscriptionsRepository;
+    
+    private readonly IUnitOfWork _unitOfWork;
+
+    public CreateSubscriptionCommandHandler(ISubscriptionsRepository subscriptionsRepository, IUnitOfWork unitOfWork)
     {
-        return Error.Failure();
+        _subscriptionsRepository = subscriptionsRepository;
+        _unitOfWork = unitOfWork;
+    }
+
+    public async Task<ErrorOr<Subscription>> Handle(CreateSubscriptionCommand request, CancellationToken cancellationToken)
+    {
+        var subscription = new Subscription()
+        {
+            Id = Guid.NewGuid()
+        };
+        
+        await _subscriptionsRepository.AddSubscription(subscription);
+        await _unitOfWork.CommitChangesAsync();
+        
+        return subscription;
+
     }
 }
