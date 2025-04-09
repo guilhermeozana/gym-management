956ccccc23931ce06ad13d197907228caaf9c1f3
Guilherme Campos <guilhermeozana@hotmail.com>
2025-04-09 08:31:13 -0300

    fix: modifying admin table

--- GymManagement.Application/Subscriptions/Commands/CreateSubscription/CreateSubscriptionCommand.cs
+++
@@ -1,7 +0,0 @@
-using MediatR;
-using ErrorOr;
-using GymManagement.Domain.Subscriptions;
-
-namespace GymManagement.Application.Subscriptions.Commands.CreateSubscription;
-
-public record CreateSubscriptionCommand(SubscriptionType SubscriptionType, Guid AdminId): IRequest<ErrorOr<Subscription>>; 
